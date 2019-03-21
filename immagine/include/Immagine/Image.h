a226b87661846349247a68725d10c519c60a3730
ismail <ismail.samir52@gmail.com>
2019-03-21 18:26:19 +0200

    Add image_padding function .. clean up code

--- immagine/include/Immagine/Image.h
+++ immagine/include/Immagine/Image.h
@@ -1,85 +1,100 @@
 #pragma once
 #include <stdint.h>
 #include <stdio.h>
+#include <assert.h>
+
 #include "Immagine/Exports.h"
-
-enum IMAGE_KIND
+namespace immagine
 {
-	KIND_NONE,
-	BMP,
-	PNG,
-	JPEG
-};
+	enum IMAGE_KIND
+	{
+		NONE,
+		BMP,
+		PNG,
+		JPEG
+	};
 
-struct Image
-{
-	unsigned char *data;
-	uint32_t width;
-	uint32_t height;
-	uint32_t depth;
-};
+	struct Image
+	{
+		unsigned char *data;
+		uint32_t width;
+		uint32_t height;
+		uint32_t depth;
 
-API_IMMAGINE Image
-image_new();
+		unsigned char&
+		operator()(size_t ix, size_t iy, size_t iz)
+		{
+			assert(ix >= 0 && ix < width  && "ERROR: Index out of range.\n");
+			assert(iy >= 0 && iy < height && "ERROR: Index out of range.\n");
+			assert(iz >= 0 && iz < depth  && "ERROR: Index out of range.\n");
 
-API_IMMAGINE Image
-image_new(uint32_t width, uint32_t height, uint32_t depth = 4);
+			size_t idx = ix + iy * width + iz * width * height;
+			return data[idx];
+		}
 
-API_IMMAGINE void
-image_free(Image& image);
+		const unsigned char&
+		operator()(size_t ix, size_t iy, size_t iz) const
+		{
+			assert(ix >= 0 && ix < width  && "ERROR: Index out of range.\n");
+			assert(iy >= 0 && iy < height && "ERROR: Index out of range.\n");
+			assert(iz >= 0 && iz < depth  && "ERROR: Index out of range.\n");
 
-inline static void
-destruct(Image& image)
-{
-	image_free(image);
+			size_t idx = ix + iy * width + iz * width * height;
+			return data[idx];
+		}
+	};
+
+	API_IMMAGINE Image
+	image_new(uint32_t width, uint32_t height, uint32_t depth = 4);
+
+	API_IMMAGINE void
+	image_free(Image& image);
+
+	inline static void
+	destruct(Image& image)
+	{
+		image_free(image);
+	}
+
+	API_IMMAGINE Image
+	image_load(const char* file_path);
+
+	API_IMMAGINE bool
+	image_save(const char* file_path, const Image& image, IMAGE_KIND kind);
+
+	API_IMMAGINE Image
+	image_red_channel(const Image& image);
+
+	API_IMMAGINE Image
+	image_green_channel(const Image& image);
+
+	API_IMMAGINE Image
+	image_blue_channel(const Image& image);
+
+	API_IMMAGINE void
+	image_histogram(const Image& image, float hist[]);
+
+	API_IMMAGINE void
+	image_histogram_red_channel(const Image& image, float hist[]);
+
+	API_IMMAGINE void
+	image_histogram_green_channel(const Image& image, float hist[]);
+
+	API_IMMAGINE void
+	image_histogram_blue_channel(const Image& image, float hist[]);
+
+	API_IMMAGINE Image
+	image_histogram_equalization(const Image& image);
+
+	API_IMMAGINE Image
+	image_brightness(const Image& image, int16_t brightness);
+
+	API_IMMAGINE Image
+	image_binarize(const Image& image, uint8_t threshold);
+
+	API_IMMAGINE Image
+	image_gray_scale(const Image& image);
+
+	API_IMMAGINE Image
+	image_padding(const Image& image, uint8_t expand, unsigned char value);
 }
-
-API_IMMAGINE Image
-image_read(const char* file_path);
-
-API_IMMAGINE bool
-image_write(const char* file_path, const Image& image, IMAGE_KIND kind);
-
-API_IMMAGINE Image
-image_binarize(const Image& image, uint8_t threshold);
-
-API_IMMAGINE Image
-image_red_channel(const Image& image);
-
-API_IMMAGINE Image
-image_green_channel(const Image& image);
-
-API_IMMAGINE Image
-image_blue_channel(const Image& image);
-
-API_IMMAGINE Image
-image_rgb_to_gray_scale(const Image& image);
-
-API_IMMAGINE Image
-image_brightness(const Image& image, int16_t brightness);
-
-inline static void
-_image_histogram_dump(const char* file_path, float data[])
-{
-	FILE *handle = fopen(file_path, "w");
-
-	for (size_t i = 0; i < 256; ++i)
-		fprintf(handle, "\n%f", data[i]);
-
-	fclose(handle);
-}
-
-API_IMMAGINE void
-image_histogram(const Image& image, float hist[]);
-
-API_IMMAGINE void
-image_histogram_red_channel(const Image& image, float hist[]);
-
-API_IMMAGINE void
-image_histogram_green_channel(const Image& image, float hist[]);
-
-API_IMMAGINE void
-image_histogram_blue_channel(const Image& image, float hist[]);
-
-API_IMMAGINE Image
-image_histogram_equalization(const Image& image);
--- immagine/src/Immagine/Image.cpp
+++ immagine/src/Immagine/Image.cpp
@@ -1,238 +1,298 @@
 #pragma once
 
 #include "Immagine/Image.h"
-#include <assert.h>
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #define STB_IMAGE_WRITE_IMPLEMENTATION
-#define STB_IMAGE_IMPLEMENTATION
-
-#include "stb_image.h"
 #include "stb_image_write.h"
 
-#define WHITE 255
-#define BLACK 0
-
-Image
-image_new()
+namespace immagine
 {
-	Image self{};
-
-	self.width = 0;
-	self.height = 0;
-	self.depth = 0;
-	self.data = nullptr;
-
-	return self;
-}
-
-Image image_new(uint32_t width, uint32_t height, uint32_t depth)
-{
-	Image self{};
-
-	self.width = width;
-	self.height = height;
-	self.depth = depth;
-	self.data = new unsigned char[width * height * depth];
-
-	return self;
-}
-
-void
-image_free(Image& self)
-{
-	self.width = 0;
-	self.height = 0;
-	self.depth = 0;
-	if(self.data) delete[] self.data;
-	self.data = nullptr;
-}
-
-Image
-image_read(const char* file_path)
-{
-	Image self{};
-	self.data = stbi_load(file_path, (int*)&self.width, (int*)&self.height, (int*)&self.depth, 0);
-	return self;
-}
-
-bool
-image_write(const char* file_path, const Image& image, IMAGE_KIND kind)
-{
-	switch (kind)
+	Image
+	image_new(uint32_t width, uint32_t height, uint32_t depth)
 	{
-	case IMAGE_KIND::BMP:
-		return stbi_write_bmp(file_path, image.width, image.height, image.depth, image.data);
+		Image self{};
 
-	case IMAGE_KIND::PNG:
-		return stbi_write_png(file_path, image.width, image.height, image.depth, image.data, 0);
+		self.width = width;
+		self.height = height;
+		self.depth = depth;
+		::malloc(width * height * depth * sizeof(unsigned char));
 
-	case IMAGE_KIND::JPEG:
-		return stbi_write_jpg(file_path, image.width, image.height, image.depth, image.data, 0);
-	
-	default:
-		assert(false && "The kind of image is wierd\n");
-		return false;
-	}
-}
-
-Image
-image_binarize(const Image & image, uint8_t threshold)
-{
-	assert(image.depth == 1 && "Image must be gray scale image\n");
-
-	Image self = image_new(image.width, image.height, image.depth);
-
-	size_t size = image.width * image.height * image.depth;
-	for (size_t i = 0; i < size; ++i)
-		self.data[i] = (image.data[i] > threshold) ? WHITE : BLACK;
-
-	return self;
-}
-
-Image
-image_red_channel(const Image & image)
-{
-	Image self = image_new(image.width, image.height, image.depth);
-
-	size_t size = image.width * image.height * image.depth;
-	for (size_t i = 0; i < size; i += image.depth)
-	{
-		self.data[i]     = image.data[i];
-		self.data[i + 1] = 0;
-		self.data[i + 2] = 0;
+		return self;
 	}
 
-	return self;
-}
-
-Image
-image_green_channel(const Image & image)
-{
-	Image self = image_new(image.width, image.height, image.depth);
-
-	size_t size = image.width * image.height * image.depth;
-	for (size_t i = 0; i < size; i += image.depth)
+	void
+	image_free(Image& self)
 	{
-		self.data[i] = 0;
-		self.data[i + 1] = image.data[i];
-		self.data[i + 2] = 0;
+		self.width = 0;
+		self.height = 0;
+		self.depth = 0;
+		if (self.data)
+			::free(self.data);
+		self.data = nullptr;
 	}
 
-	return self;
-}
-
-Image
-image_blue_channel(const Image & image)
-{
-	Image self = image_new(image.width, image.height, image.depth);
-
-	size_t size = image.width * image.height * image.depth;
-	for (size_t i = 0; i < size; i += image.depth)
+	Image
+	image_load(const char* file_path)
 	{
-		self.data[i] = 0;
-		self.data[i + 1] = 0;
-		self.data[i + 2] = image.data[i];
+		Image self{};
+		self.data = stbi_load(file_path, (int*)&self.width, (int*)&self.height, (int*)&self.depth, 0);
+		return self;
 	}
 
-	return self;
-}
-
-Image
-image_rgb_to_gray_scale(const Image & image)
-{
-	Image self = image_new(image.width, image.height, 1);
-
-	size_t size = image.width * image.height * 1;
-	for (size_t i = 0, j = 0; i < size; ++i, j += image.depth)
+	bool
+	image_save(const char* file_path, const Image& image, IMAGE_KIND kind)
 	{
-		// using The luminosity method
-		unsigned char gray_scale = ((0.21 * image.data[j]) + (0.72 * image.data[j + 1]) + (0.07 * image.data[j + 2])) / 3;
-		self.data[i] = gray_scale;
+		switch (kind)
+		{
+		case IMAGE_KIND::BMP:
+			return stbi_write_bmp(file_path, image.width, image.height, image.depth, image.data);
+
+		case IMAGE_KIND::PNG:
+			return stbi_write_png(file_path, image.width, image.height, image.depth, image.data, 0);
+
+		case IMAGE_KIND::JPEG:
+			return stbi_write_jpg(file_path, image.width, image.height, image.depth, image.data, 0);
+
+		default:
+			assert(false && "The kind of image is wierd\n");
+			return false;
+		}
 	}
 
-	return self;
-}
-
-Image
-image_brightness(const Image & image, int16_t brightness)
-{
-	Image self = image_new(image.width, image.height, image.depth);
-
-	size_t size = image.width * image.height * image.depth;
-	for (size_t i = 0; i < size; ++i)
+	Image
+	image_red_channel(const Image & image)
 	{
-		int16_t tmp = image.data[i] + brightness;
-		self.data[i] = (tmp < 0) ? 0 : (tmp > 255) ? 255 :tmp;
+		Image self = image_new(image.width, image.height, image.depth);
+
+		size_t size = image.width * image.height * image.depth;
+		for (size_t i = 0; i < size; i += image.depth)
+		{
+			self.data[i] = image.data[i];
+			self.data[i + 1] = 0;
+			self.data[i + 2] = 0;
+		}
+
+		return self;
 	}
 
-	return self;
-}
+	Image
+	image_green_channel(const Image & image)
+	{
+		Image self = image_new(image.width, image.height, image.depth);
 
-void
-image_histogram(const Image& image, float hist[])
-{
-	size_t size = image.width * image.height * image.depth;
-	for (size_t i = 0; i < size; ++i)
-		hist[image.data[i]] += 1;
+		size_t size = image.width * image.height * image.depth;
+		for (size_t i = 0; i < size; i += image.depth)
+		{
+			self.data[i] = 0;
+			self.data[i + 1] = image.data[i];
+			self.data[i + 2] = 0;
+		}
 
-	for (size_t i = 0; i < 256; ++i)
-		hist[i] = (float)hist[i] / (float)size;
-}
+		return self;
+	}
 
-void
-image_histogram_red_channel(const Image & image, float hist[])
-{
-	size_t size = image.width * image.height * image.depth;
-	for (size_t i = 0; i < size; i+= image.depth)
-		hist[image.data[i]] += 1;
+	Image
+	image_blue_channel(const Image & image)
+	{
+		Image self = image_new(image.width, image.height, image.depth);
 
-	for (size_t i = 0; i < 256; ++i)
-		hist[i] = (float)hist[i] / (float)size;
-}
+		size_t size = image.width * image.height * image.depth;
+		for (size_t i = 0; i < size; i += image.depth)
+		{
+			self.data[i] = 0;
+			self.data[i + 1] = 0;
+			self.data[i + 2] = image.data[i];
+		}
 
-void
-image_histogram_green_channel(const Image & image, float hist[])
-{
-	size_t size = image.width * image.height * image.depth;
-	for (size_t i = 1; i < size; i += image.depth)
-		hist[image.data[i]] += 1;
+		return self;
+	}
 
-	for (size_t i = 0; i < 256; ++i)
-		hist[i] = (float)hist[i] / (float)size;
-}
+	inline static void
+	_image_histogram_dump(const char* file_path, float data[])
+	{
+		FILE *handle = fopen(file_path, "w");
 
-void
-image_histogram_blue_channel(const Image & image, float hist[])
-{
-	size_t size = image.width * image.height * image.depth;
-	for (size_t i = 2; i < size; i += image.depth)
-		hist[image.data[i]] += 1;
+		for (size_t i = 0; i < 256; ++i)
+			fprintf(handle, "\n%f", data[i]);
 
-	for (size_t i = 0; i < 256; ++i)
-		hist[i] = (float)hist[i] / (float)size;
-}
+		fclose(handle);
+	}
 
-Image
-image_histogram_equalization(const Image& image)
-{
-	Image self = image_new(image.width, image.height, image.depth);
+	void
+	image_histogram(const Image& image, float hist[])
+	{
+		size_t size = image.width * image.height * image.depth;
+		for (size_t i = 0; i < size; ++i)
+			hist[image.data[i]] += 1;
 
-	// calculate PMF(Probability Mass Function)
-	float hist[256] = {};
-	image_histogram(image, hist);
+		for (size_t i = 0; i < 256; ++i)
+			hist[i] = (float)hist[i] / (float)size;
+	}
 
-	// calculate CDF(Cumulative Distributive Function)
-	for (size_t i = 1; i < 256; ++i)
-		hist[i] += hist[i - 1];
+	void
+	image_histogram_red_channel(const Image & image, float hist[])
+	{
+		size_t size = image.width * image.height * image.depth;
+		for (size_t i = 0; i < size; i += image.depth)
+			hist[image.data[i]] += 1;
 
-	// calculate CDF according to gray levels
-	for (size_t i = 1; i < 256; ++i)
-		hist[i] = (255 * hist[i] + 0.5f);
+		for (size_t i = 0; i < 256; ++i)
+			hist[i] = (float)hist[i] / (float)size;
+	}
 
-	// mapping new values with according pixel
-	size_t size = image.width * image.height * image.depth;
-	for (size_t i = 0; i < size; ++i)
-		self.data[i] = hist[image.data[i]];
+	void
+	image_histogram_green_channel(const Image & image, float hist[])
+	{
+		size_t size = image.width * image.height * image.depth;
+		for (size_t i = 1; i < size; i += image.depth)
+			hist[image.data[i]] += 1;
 
-	return self;
+		for (size_t i = 0; i < 256; ++i)
+			hist[i] = (float)hist[i] / (float)size;
+	}
+
+	void
+	image_histogram_blue_channel(const Image & image, float hist[])
+	{
+		size_t size = image.width * image.height * image.depth;
+		for (size_t i = 2; i < size; i += image.depth)
+			hist[image.data[i]] += 1;
+
+		for (size_t i = 0; i < 256; ++i)
+			hist[i] = (float)hist[i] / (float)size;
+	}
+
+	Image
+	image_histogram_equalization(const Image& image)
+	{
+		Image self = image_new(image.width, image.height, image.depth);
+
+		// calculate PMF(Probability Mass Function)
+		float hist[256] = {};
+		image_histogram(image, hist);
+
+		// calculate CDF(Cumulative Distributive Function)
+		for (size_t i = 1; i < 256; ++i)
+			hist[i] += hist[i - 1];
+
+		// calculate CDF according to gray levels
+		for (size_t i = 1; i < 256; ++i)
+			hist[i] = (255 * hist[i] + 0.5f);
+
+		// mapping new values with according pixel
+		size_t size = image.width * image.height * image.depth;
+		for (size_t i = 0; i < size; ++i)
+			self.data[i] = hist[image.data[i]];
+
+		return self;
+	}
+
+	Image
+	image_brightness(const Image & image, int16_t brightness)
+	{
+		Image self = image_new(image.width, image.height, image.depth);
+
+		size_t size = image.width * image.height * image.depth;
+		for (size_t i = 0; i < size; ++i)
+		{
+			int16_t tmp = image.data[i] + brightness;
+			self.data[i] = (tmp < 0) ? 0 : (tmp > 255) ? 255 : tmp;
+		}
+
+		return self;
+	}
+
+	Image
+	image_binarize(const Image & image, uint8_t threshold)
+	{
+		assert(image.depth == 1 && "Image must be gray scale image\n");
+
+		Image self = image_new(image.width, image.height, image.depth);
+
+		size_t size = image.width * image.height * image.depth;
+		for (size_t i = 0; i < size; ++i)
+			self.data[i] = (image.data[i] > threshold) ? 255 : 0;
+
+		return self;
+	}
+
+	Image
+	image_gray_scale(const Image & image)
+	{
+		Image self = image_new(image.width, image.height, 1);
+
+		size_t size = image.width * image.height * 1;
+		for (size_t i = 0, j = 0; i < size; ++i, j += image.depth)
+		{
+			// using The luminosity method
+			unsigned char gray_scale = ((0.21 * image.data[j]) + (0.72 * image.data[j + 1]) + (0.07 * image.data[j + 2])) / 3;
+			self.data[i] = gray_scale;
+		}
+
+		return self;
+	}
+
+	inline static Image
+	_image_pad_1d(const Image& image, uint8_t expand, unsigned char value)
+	{
+		Image self = image_new(image.width + 2 * expand, 1, 0);
+
+		::memset(self.data, value, self.width);
+
+		for (int i = expand; i < self.width - expand; ++i)
+			self(i, 0, 0) = image(i - expand, 0, 0);
+
+		return self;
+	}
+
+	inline static Image
+	_image_pad_2d(const Image& image, uint8_t expand, unsigned char value)
+	{
+		Image self = image_new(image.width + 2 * expand, image.height + 2 * expand, 1);
+
+		::memset(self.data, value, self.width * self.height);
+
+		for (int j = expand; j < self.height - expand; ++j)
+			for (int i = expand; i < self.width - expand; ++i)
+				self(i, j, 0) = image(i - expand, j - expand, 0);
+
+		return self;
+	}
+
+	inline static Image
+	_image_pad_3d(const Image& image, uint8_t expand, unsigned char value)
+	{
+		Image self = image_new(image.width + 2 * expand, image.height + 2 * expand, image.depth + 2 * expand);
+
+		::memset(self.data, value, self.width * self.height * self.depth);
+
+		for (int k = expand; k < self.depth - expand; ++k)
+			for (int j = expand; j < self.height - expand; ++j)
+				for (int i = expand; i < self.width - expand; ++i)
+					self(i, j, k) = image(i - expand, j - expand, k - expand);
+
+		return self;
+	}
+
+	Image
+	image_padding(const Image& image, uint8_t expand, unsigned char value)
+	{
+		switch (image.depth)
+		{
+		case 0:
+			return _image_pad_1d(image, expand, value);
+		case 1:
+			return _image_pad_2d(image, expand, value);
+		case 2:
+		case 3:
+		case 4:
+			return _image_pad_3d(image, expand, value);
+		default:
+			assert(false && "Unsupported image format");
+			return image;
+		}
+	}
 }
