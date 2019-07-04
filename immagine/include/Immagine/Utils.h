3239efcfae086e2f5bded2b6a65af314df3fdfbd
ismail <ismail.samir52@gmail.com>
2019-07-05 00:21:23 +0200

    fix the trick issue

--- immagine/include/Immagine/Utils.h
+++ immagine/include/Immagine/Utils.h
@@ -8,7 +8,7 @@ namespace immagine
 	inline static int8_t
 	node_is_leaf(const Image& image, Region region)
 	{
-		for (size_t i = region.y; i < region.height; ++i)
+		for (size_t i = region.y; i < region.y + region.height; ++i)
 			for (size_t j = region.x; j < region.x + region.width; ++j)
 				if (image(i, j, 0) != image(region.y, region.x, 0))
 					return -1;
