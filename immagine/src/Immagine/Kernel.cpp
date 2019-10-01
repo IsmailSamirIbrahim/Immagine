1f0cbe116f7a1fe07e695fb0b6974d139f2a47f1
Ismail <ismail.samir@symbyo.com>
2019-10-01 13:54:51 +0200

    ay 7aga

--- immagine/src/Immagine/Kernel.cpp
+++ immagine/src/Immagine/Kernel.cpp
@@ -33,33 +33,50 @@ namespace immagine
 		kernel.data = nullptr;
 	}
 
+	inline static void
+	kernel_conv_horizental(const Kernel& kernel, const Image& image, Image& self)
+	{
+		uint32_t offset = kernel.width / 2;
+		for (uint8_t k = 0; k < image.channels; ++k)
+			for (size_t i = 0; i < image.height; ++i)
+				for (size_t j = 0; j < image.width - kernel.width; ++j) {
+					float val = 0.0f;
+					for (size_t x = 0; x < kernel.width; ++x)
+						val += image(i, j + x, k) * kernel.data[x];
+					self(i, j + offset, k) = uint8_t(val);
+				}
+	}
+
+	inline static void
+	kernel_conv_vertical(const Kernel& kernel, const Image& image, Image& self)
+	{
+		uint32_t offset = kernel.height / 2;
+		for (uint8_t k = 0; k < image.channels; ++k)
+			for (size_t i = 0; i < image.height - kernel.height; ++i)
+				for (size_t j = 0; j < image.width; ++j) {
+					float val = 0.0f;
+					for (size_t x = 0; x < kernel.height; ++x)
+						val += image(i + x, j, k) * kernel.data[x];
+					self(i + offset, j, k) = uint8_t(val);
+				}
+	}
+
 	Image
 	kernel_conv(const Kernel& kernel, const Image& image)
 	{
-		uint32_t offset = kernel.width / 2;
-
-		Image padded_image = image_pad(image, offset, offset, 0);
-
 		Image self = image_new(image.width, image.height, image.channels);
 
-		size_t nh = padded_image.height - (kernel.height / 2);
-		size_t nw = padded_image.width - (kernel.width / 2);
-
-		for (uint8_t k = 0; k < image.channels; ++k){
-			for (size_t i = (kernel.height / 2); i < nh; ++i){
-				for (size_t j = (kernel.width / 2); j < nw; ++j){
-					float val = 0.0f;
-					for (size_t y = 0; y < kernel.height; ++y){
-						for (size_t x = 0; x < kernel.width; ++x){
-							val += kernel(y, x) * padded_image(i + y - kernel.height / 2, j + x - kernel.width / 2, k);
-						}
-					}
-					self(i - offset, j - offset, k) = uint8_t(val);
-				}
-			}
-		}
-
-		image_free(padded_image);
+		Kernel kernelh = kernel_new(51, 1);
+		float val = (1.0f / (kernelh.width * kernelh.height));
+		for (int i = 0; i < kernelh.width * kernelh.height; ++i)
+			kernelh.data[i] = val;
+		kernel_conv_horizental(kernelh, image, self);
+		
+		Kernel kernelv = kernel_new(1, 51);
+		val = (1.0f / (kernelv.width * kernelv.height));
+		for (int i = 0; i < kernelv.width * kernelv.height; ++i)
+			kernelv.data[i] = val;
+		kernel_conv_vertical(kernelv, image, self);
 
 		return self;
 	}
