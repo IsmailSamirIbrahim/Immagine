19d6a41f756c8de01672c67a940f1d05ee82e217
Ismail Samir Ismail <ismail.samir52@gmail.com>
2019-10-03 11:40:24 +0200

    implement Mean filter using "Summed Area Table" (#4)
    * ay 7aga
    
    * Initial commit
    
    * implement Mean filter using summed_area_table
    
    * rename the functions

--- playground/src/playground.cpp
+++ playground/src/playground.cpp
@@ -13,26 +13,24 @@ using namespace std;
 int
 main(int argc, char** argv)
 {
-	Image image = image_load("D:/Ismail/fruits.bmp");
+	Image image = image_load("D:/Ismail/images/22.jpg");
 	
 	auto start = high_resolution_clock::now();
 
 	//type code here.
 	
-	Kernel kernel = kernel_new(21, 21);
+	Kernel kernel = kernel_new(201, 201);
 	float val = (1.0f / (kernel.width * kernel.height));
 	for (int i = 0; i < kernel.width * kernel.height; ++i)
 		kernel.data[i] = val;
 
 	Image img1 = kernel_conv(kernel, image);
 
-	//Image img1 = image_pad(image, 50, 5, 0);
-
 	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<seconds>(stop - start);
-	printf("Time = %lld  millisecond\n", duration.count());
+	auto duration = duration_cast<milliseconds>(stop - start);
+	printf("Time = %lld  milliseconds\n", duration.count());
 
-	image_save("D:/Ismail/convolved_image.bmp", img1, FORMAT::BMP);
+	image_save("D:/Ismail/images/convolved_image.bmp", img1, FORMAT::BMP);
 
 	image_free(image);
 	image_free(img1);
