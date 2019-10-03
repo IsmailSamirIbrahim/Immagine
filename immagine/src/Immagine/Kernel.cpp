2dc05fac8d0bad803317ae40430a67ba29785a7f
Ismail <ismail.samir@symbyo.com>
2019-10-03 10:46:49 +0200

    implement Mean filter using summed_area_table

--- immagine/src/Immagine/Kernel.cpp
+++ immagine/src/Immagine/Kernel.cpp
@@ -1,5 +1,8 @@
 #include "Immagine/Kernel.h"
 #include <memory>
+#include <vector>
+
+using namespace std;
 
 namespace immagine
 {
@@ -72,22 +75,60 @@ namespace immagine
 				}
 	}
 
+	inline static void
+	calculate_summed_area(const Image& image, vector<vector<vector<uint32_t>>>& summed_table)
+	{
+		uint32_t value = 0;
+		for (uint8_t k = 0; k < image.channels; ++k) {
+			for (size_t i = 0; i < image.height; ++i) {
+				for (size_t j = 0; j < image.width; ++j) {
+					value = image(i, j, k);
+					/* compute summed-area table value, avoid out of bounce access */
+					if (i > 0)
+						value += summed_table[i - 1][j][k];
+					if (j > 0)
+						value += summed_table[i][j - 1][k];
+					if (j > 0 && i > 0)
+						value -= summed_table[i - 1][j - 1][k];
+					/* save computed value in summed-area table */
+					summed_table[i][j][k] = value;
+				}
+			}
+		}
+	}
+
+	inline static uint32_t
+	calcMean(size_t i, size_t j, size_t k, size_t kernel_height, size_t kernel_width, const vector<vector<vector<uint32_t>>>& summed_table)
+	{
+		uint32_t mean = 0;
+		if (i > 0 && j > 0)
+			mean += summed_table[i - 1][j - 1][k];
+		if (j > 0)
+			mean -= summed_table[i - 1 + kernel_height][j - 1][k];
+		if (i > 0)
+			mean -= summed_table[i - 1][j - 1 + kernel_width][k];
+
+		mean += summed_table[i + kernel_height - 1][j + kernel_width - 1][k];
+		mean /= (kernel_width * kernel_height);
+
+		return mean;
+	}
+
 	Image
 	kernel_conv(const Kernel& kernel, const Image& image)
 	{
 		Image self = image_new(image.width, image.height, image.channels);
 
-		Kernel kernelh = kernel_new(51, 1);
-		float val = (1.0f / (kernelh.width * kernelh.height));
-		for (int i = 0; i < kernelh.width * kernelh.height; ++i)
-			kernelh.data[i] = val;
-		kernel_conv_horizental(kernelh, image, self);
-		
-		Kernel kernelv = kernel_new(1, 51);
-		val = (1.0f / (kernelv.width * kernelv.height));
-		for (int i = 0; i < kernelv.width * kernelv.height; ++i)
-			kernelv.data[i] = val;
-		kernel_conv_vertical(kernelv, image, self);
+		vector<vector<vector<uint32_t>>> summed_table(image.height, vector<vector<uint32_t>>(image.width, vector<uint32_t>(image.channels)));
+		calculate_summed_area(image, summed_table);
+
+		size_t width_offset = kernel.width / 2;
+		size_t height_offset = kernel.height / 2;
+
+		for (uint8_t k = 0; k < image.channels; ++k)
+			for (size_t i = 0; i < image.height - kernel.height; ++i)
+				for (size_t j = 0; j < image.width - kernel.width; ++j)
+					self(i + height_offset, j + width_offset, k) = calcMean(i, j, k, kernel.height, kernel.width, summed_table);
 
 		return self;
 	}
--- playground/src/playground.cpp
+++ playground/src/playground.cpp
@@ -13,13 +13,13 @@ using namespace std;
 int
 main(int argc, char** argv)
 {
-	Image image = image_load("D:/Ismail/images/fruits.bmp");
+	Image image = image_load("D:/Ismail/images/22.jpg");
 	
 	auto start = high_resolution_clock::now();
 
 	//type code here.
 	
-	Kernel kernel = kernel_new(1, 51);
+	Kernel kernel = kernel_new(201, 201);
 	float val = (1.0f / (kernel.width * kernel.height));
 	for (int i = 0; i < kernel.width * kernel.height; ++i)
 		kernel.data[i] = val;
@@ -27,8 +27,8 @@ main(int argc, char** argv)
 	Image img1 = kernel_conv(kernel, image);
 
 	auto stop = high_resolution_clock::now();
-	auto duration = duration_cast<seconds>(stop - start);
-	printf("Time = %lld  millisecond\n", duration.count());
+	auto duration = duration_cast<milliseconds>(stop - start);
+	printf("Time = %lld  milliseconds\n", duration.count());
 
 	image_save("D:/Ismail/images/convolved_image.bmp", img1, FORMAT::BMP);
 
