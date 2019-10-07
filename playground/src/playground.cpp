d1521334143d79f27a7001bcf7c14dcf7ab75916
Ismail Samir Ismail <ismail.samir52@gmail.com>
2019-10-07 22:02:59 +0200

    Cleanup (#10)
    * cleanup box-filter and gaussian filter and horixental and vertical filters
    
    * cleanup median filter

--- playground/src/playground.cpp
+++ playground/src/playground.cpp
@@ -6,7 +6,8 @@
 
 #include <chrono>
 #include <string>
-#include <vld.h>
+
+//#include <vld.h>
 
 using namespace std::chrono;
 using namespace immagine;
@@ -16,20 +17,20 @@ int
 main(int argc, char** argv)
 {
 
-	string file_path = string(IMAGE_DIR) + string("/images/fruits.bmp");
+	string file_path = string(IMAGE_DIR) + string("/images/10.png");
 	Image image = image_load(file_path.c_str());
 	
 	auto start = high_resolution_clock::now();
 
 	//type code here.
 	
-	Image img = image_binarize(image);
+	Image img = image_median_filter(image, 11, 11);
 
 	auto stop = high_resolution_clock::now();
 	auto duration = duration_cast<seconds>(stop - start);
 	printf("Time = %lld  seconds\n", duration.count());
 
-	string out_path = string(IMAGE_DIR) + string("/images/binraized.bmp");
+	string out_path = string(IMAGE_DIR) + string("/images/result.bmp");
 	image_save(out_path.c_str(), img, IMAGE_FORMAT::BMP);
 
 	image_free(image);
