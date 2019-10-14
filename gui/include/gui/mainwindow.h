ee31e365a8d8429cf6f4b5a9a33cee39a069b883
Ismail <ismail.samir@symbyo.com>
2019-10-14 13:44:54 +0200

    Create ui

--- gui/include/gui/mainwindow.h
+++ gui/include/gui/mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QImage>
+#include <Immagine/Image.h>
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,12 @@ public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionOpen_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QImage mImage;
 };
 
 #endif // MAINWINDOW_H
--- gui/src/gui/mainwindow.cpp
+++ gui/src/gui/mainwindow.cpp
@@ -1,5 +1,15 @@
 #include "gui/mainwindow.h"
 #include "gui/ui_mainwindow.h"
+#include "Immagine/Image.h"
+
+#include <QImageReader>
+#include <QFileDialog>
+#include <QString>
+#include <iostream>
+#include <string>
+
+using namespace immagine;
+using namespace std;
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +21,16 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString tmp = QFileDialog::getOpenFileName(this, "Open File", "C://");
+    string tmp1 = tmp.toStdString();
+    const char* file_path = tmp1.c_str();
+
+    Image image = image_load(file_path);
+
+    this->mImage = QImage(image.data, image.width, image.height, QImage::Format_RGB30);
+    this->ui->label->setPixmap(QPixmap::fromImage(this->mImage));
 }
